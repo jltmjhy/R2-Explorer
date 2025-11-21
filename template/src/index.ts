85a6717c7abcea5208b776aa97994ced5d6be8ad
jltmjhy <65304360+jltmjhy@users.noreply.github.com>
2025-11-21 18:36:58 +0800

    Change cfAccessTeamName in R2Explorer configuration

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,4 +1,4 @@
 import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
-export default R2Explorer({ readonly: false, cfAccessTeamName: 'r2explorer' });
+export default R2Explorer({ readonly: false, cfAccessTeamName: 'jltmjhy' });
